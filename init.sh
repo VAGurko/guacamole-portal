3043cb16ee3331459928f53fc62f6a4c6c44f9f2
VAGurko <va.gurko@yandex.ru>
2025-01-16 10:48:54 +0400

    Init
    Signed-off-by: VAGurko <va.gurko@yandex.ru>

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+echo "Reset ALL DATA !!!?"
+pause
+
+cp .env.example .env
+cp .postgres_env.example .postgres_env
+
+echo "Change the envevironement in files .env , .postgres_env and start containers docker-compose up -d"
+echo "After launching the containers, log in to your application https://your-site-name:7443 using guacadmin username and password."
+echo "Don't forget to change your password after logging in for the first time."
+pause
