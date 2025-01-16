83fc0eb1d5751dbe23620dd6685cd08e24c6e80e
VAGurko <va.gurko@yandex.ru>
2025-01-16 11:19:21 +0400

    Init
    Signed-off-by: VAGurko <va.gurko@yandex.ru>

--- init.sh
+++ init.sh
@@ -2,17 +2,13 @@
 echo "Reset ALL DATA !!!?"
 sleep 1h
 
-rm -rf ./conf/
-rm -rf ./nginx/
-
-mkdir -p -m 770 "./conf/guacamole/"
 mkdir -p -m 770 "./nginx/ssl/"
-mkdir -p -m 770 "./nginx/templates/"
-cp -f "./conf/guacamole/guacamole.conf" "./conf/guacamole/"
 cp -f .env.example .env
 cp -f .postgres_env.example .postgres_env
+cp -f ./nginx/templates/nginx.conf.template.example ./nginx/templates/nginx.conf.template
 
-echo "Change the envevironement in files .env , .postgres_env and start containers docker-compose up -d"
-echo "After launching the containers, log in to your application https://your-site-name:7443 using guacadmin username and password."
+echo "Copy your certificate files fullchain.pem and privkey.pem to ./nginx/ssl/"
+echo "Change the envevironement in files .env , .postgres_env , nginx/templates/nginx.conf.template and start containers docker-compose up -d"
+echo "After launching the containers, log in to your application https://your-site:7443 using guacadmin username and password."
 echo "Don't forget to change your password after logging in for the first time."
 sleep 1h
