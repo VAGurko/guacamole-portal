488084595f4ed673ae89d552cf56c7e4b556c260
VAGurko <va.gurko@yandex.ru>
2025-01-16 11:00:37 +0400

    Init
    Signed-off-by: VAGurko <va.gurko@yandex.ru>

--- init.sh
+++ init.sh
@@ -2,8 +2,15 @@
 echo "Reset ALL DATA !!!?"
 sleep 1h
 
-cp .env.example .env
-cp .postgres_env.example .postgres_env
+rm -rf ./conf/
+rm -rf ./nginx/
+
+mkdir -p -m 770 "./conf/guacamole/"
+mkdir -p -m 770 "./nginx/ssl/"
+mkdir -p -m 770 "./nginx/templates/"
+cp -f "./conf/guacamole/guacamole.conf" "./conf/guacamole/"
+cp -f .env.example .env
+cp -f .postgres_env.example .postgres_env
 
 echo "Change the envevironement in files .env , .postgres_env and start containers docker-compose up -d"
 echo "After launching the containers, log in to your application https://your-site-name:7443 using guacadmin username and password."
